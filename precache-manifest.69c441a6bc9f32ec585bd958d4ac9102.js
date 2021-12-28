0a7ad829524eacafdbd3c2bbf2bc126ff3f0c97a
swapcp1000 <swapcp.1000@gmail.com>
2021-12-28 22:16:18 +0530

    Updates

---
+++ precache-manifest.69c441a6bc9f32ec585bd958d4ac9102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230da3ecb80312d2e35aad42922c9079",
+    "url": "/react_quiz/index.html"
+  },
+  {
+    "revision": "f6b163c522d874bec361",
+    "url": "/react_quiz/static/css/main.270bfac1.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/react_quiz/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_quiz/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b163c522d874bec361",
+    "url": "/react_quiz/static/js/main.f85a005d.chunk.js"
+  },
+  {
+    "revision": "5e8dc92abaca55c2336d",
+    "url": "/react_quiz/static/js/runtime-main.11ed617b.js"
+  }
+]);
